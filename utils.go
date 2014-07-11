6fbe9883fb34f7c5772126b6cab53dd417645f1e
Jordan Santell <jsantell@gmail.com>
2014-07-10 18:10:27 -0700

    Add utils for formatting messages

--- utils.go
+++ utils.go
@@ -1,11 +1,59 @@
 package main
 
-const MAX_LENGTH = 100
+import (
+	"github.com/google/go-github/github"
+)
+import "regexp"
 
-func FormatMessage(s string) string {
-	if len(s) < MAX_LENGTH {
-		return s
+// MAX_LENGTH determines how long a commit message can be after trimmed,
+// while leaving room for the URL and trailing ellipses to remain
+// under 140 characters.
+const MAX_LENGTH = 112
+
+// TODO probably can group these regexps together in some cases, parsing out chunks
+// of a commit message
+var bugRegExp = regexp.MustCompile("(?i)[\\s]*bug [\\d]{4,9}[:\\-\\s]*")
+var reviewRegExp = regexp.MustCompile("(?i)\\W*[\\s]+(a|r)=[\\s\\w,]+$")
+
+var bugNumberRegExp = regexp.MustCompile("^(?i)[\\s]*bug ([\\d]{1,9})")
+var changesetRegExp = regexp.MustCompile("^(?i)Backed out changeset")
+var mergeRegExp = regexp.MustCompile("^(?i)merge ")
+var bumpRegExp = regexp.MustCompile("^(?i)Bumping (gaia|mani)")
+
+// Replaces bug number and reviewd comments from string.
+func CleanMessage(str string) string {
+	str = bugRegExp.ReplaceAllLiteralString(str, "")
+	str = reviewRegExp.ReplaceAllLiteralString(str, "")
+	return str
+}
+
+// Creates a Bugzilla URI from a commit message.
+func CreateBugzillaURL(str string) string {
+	result := bugNumberRegExp.FindStringSubmatch(str)
+	if len(result) == 2 {
+		return "http://bugzil.la/" + result[1]
 	} else {
-		return s[0:MAX_LENGTH] + "..."
+		return ""
 	}
+}
+
+// Takes a commit and formats it into a digestable tweet.
+func FormatMessage(commit github.RepositoryCommit) string {
+	message := *commit.Commit.Message
+	url := CreateBugzillaURL(message)
+	message = CleanMessage(message)
+
+	if len(message) > MAX_LENGTH {
+		message = message[0:MAX_LENGTH] + "..."
+	}
+
+	return message + " " + url
+}
+
+// Returns a boolean indicating whether or not this commit message
+// is useful, ignoring merges, backouts and automated commits.
+func IsValidCommit(message string) bool {
+	return !changesetRegExp.MatchString(message) &&
+		!mergeRegExp.MatchString(message) &&
+		!bumpRegExp.MatchString(message)
 }
---
+++ utils_test.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-github/github"
+)
+
+func createCommitMessage(message string) github.RepositoryCommit {
+	repoCommit := new(github.RepositoryCommit)
+	commit := new(github.Commit)
+	commit.Message = &message
+	repoCommit.Commit = commit
+	return *repoCommit
+}
+
+func TestCleanMessage(t *testing.T) {
+	messages := [][]string{
+		[]string{
+			"Bug 1034670 - The canvas graphs should draw the background separately, r=pbrosset",
+			"The canvas graphs should draw the background separately",
+		},
+		[]string{
+			"Bug 1034670 - The canvas graphs should draw the background separately, a=pbrosset",
+			"The canvas graphs should draw the background separately",
+		},
+		[]string{
+			"Bug 1343: Support asm.js frames in SavedStacks. r=luke ",
+			"Support asm.js frames in SavedStacks",
+		},
+		[]string{
+			" Bug 1034668-The `getMappedSelection` method for all canvas graphs should clamp the selection bounds, r=pbrosset,someoneelse, space",
+			"The `getMappedSelection` method for all canvas graphs should clamp the selection bounds",
+		},
+	}
+
+	for _, duple := range messages {
+		if CleanMessage(duple[0]) != duple[1] {
+			t.Error("Expected '" + duple[0] + "' to clean into '" + duple[1] + "'")
+		}
+	}
+}
+
+func TestIsValidCommit(t *testing.T) {
+	invalid := []string{
+		"merge fx-team to mozilla-central a=merge",
+		"Merge mozilla-central to fx-team",
+		"Bumping manifests a=b2g-bump",
+		"Bumping gaia.json for 2 gaia revision(s) a=gaia-bump",
+		"Backed out changeset e01dbdf8a218 (bug 1016613)",
+	}
+
+	valid := []string{
+		"Bug 1034670 - The canvas graphs should draw the background separately, r=pbrosset",
+		"Bug 1343: Support asm.js frames in SavedStacks. r=luke",
+	}
+
+	for _, str := range invalid {
+		if IsValidCommit(str) {
+			t.Error("Expected " + str + " to be an invalid commit.")
+		}
+	}
+
+	for _, str := range valid {
+		if !IsValidCommit(str) {
+			t.Error("Expected " + str + " to be a valid commit.")
+		}
+	}
+}
+
+func TestCreateBugzillaURL(t *testing.T) {
+	messages := [][]string{
+		[]string{
+			"Bug 1034670 - The canvas graphs should draw the background separately, r=pbrosset",
+			"http://bugzil.la/1034670",
+		},
+		[]string{
+			"Bug 1343: Support asm.js frames in SavedStacks. r=luke ",
+			"http://bugzil.la/1343",
+		},
+		[]string{
+			" Bug 980506-The `getMappedSelection` method for all canvas graphs should clamp the selection bounds, r=pbrosset,someoneelse, space",
+			"http://bugzil.la/980506",
+		},
+	}
+
+	for _, duple := range messages {
+		if CreateBugzillaURL(duple[0]) != duple[1] {
+			t.Error("Expected '" + duple[0] + "' to have a URL: " + duple[1])
+		}
+	}
+
+	if CreateBugzillaURL("merge not a bug number") != "" {
+		t.Error("Expected invalid commit message to return empty string.")
+	}
+}
+
+func TestFormatMessage(t *testing.T) {
+	messages := [][]string{
+		[]string{
+			"Bug 1034670 - The canvas graphs should draw the background separately, r=pbrosset",
+			"The canvas graphs should draw the background separately http://bugzil.la/1034670",
+		},
+		[]string{
+			"Bug 1034670: The canvas graphs should draw the background separately and one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen, r=pbrosset",
+			"The canvas graphs should draw the background separately and one two three four five six seven eight nine ten ele... http://bugzil.la/1034670",
+		},
+	}
+
+	for _, duple := range messages {
+		commit := createCommitMessage(duple[0])
+		formatted := FormatMessage(commit)
+		if formatted != duple[1] {
+			t.Error("Expected formatted message\n'" + formatted + "'\nto have been formatted to\n'" + duple[1] + "'")
+		}
+	}
+}
