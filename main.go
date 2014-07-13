776634b185a01cc5f36de74ddce0e30094c9b230
Jordan Santell <jsantell@gmail.com>
2014-07-12 21:51:10 -0700

    Wire up sequence to take action on valid commits

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/jsantell/go-githubstream"
 )
 
-const FREQUENCY = time.Hour
+const FREQUENCY = time.Hour * 1
 const OWNER = "mozilla"
 const REPO = "gecko-dev"
 const BRANCH = "fx-team"
@@ -15,26 +16,42 @@ const DB_NAME = "__db.txt"
 
 var TOKEN string = os.Getenv("FX_DEVTOOLS_BOT_GITHUB_TOKEN")
 
-/*
-		if !GetSHA(DB_NAME, *commit.SHA) {
-			AddSHA(DB_NAME, *commit.SHA)
-			message := FormatMessage(commit)
-			fmt.Println("Tweeting: ", message)
-			_, err := Tweet(message)
-			if err != nil {
-				//panic(err)
-			}
-		}
-	}
-}
-*/
 func main() {
 	ghs := githubstream.NewGithubStream(FREQUENCY, OWNER, REPO, BRANCH, TOKEN)
 
 	for commits := range ghs.Start() {
+		fmt.Println(time.Now().Local())
+		fmt.Println("Fetched commits: ", len(commits))
 		// Filter out already used commits from DB_NAME, and invalid commits
 		// like merges, changesets, automated commits to store on overhead of querying
 		// bugzilla
-		commits = FilterCommits(DB_NAME, commits)
+		filtered := FilterCommits(DB_NAME, commits)
+		fmt.Println("Non-merge/changeset/automated commits: ", len(filtered))
+
+		for _, commit := range filtered {
+			c, err := NewBzCommit(&commit)
+
+			// If error created during creation of BzCommit,
+			// Bugzilla couldn't be queried from either not finding a bug in the
+			// commit message, or the API being down, or something else. Whatever the case
+			// just ignore for now.
+			if err != nil {
+				fmt.Println("Failed creating BzCommit: " + c.Message)
+				continue
+			}
+
+			// In this case if it's valid, meaning in the Developer Tools component
+			// in this case, tweet it.
+			if c.IsValid() {
+				fmt.Println("Tweeting Valid Commit: " + c.Message)
+				_, err := Tweet(c.FormatMessage())
+
+				if err == nil {
+					fmt.Println("Adding SHA for " + c.Message)
+					AddSHA(DB_NAME, c.SHA)
+				}
+			}
+		}
+
 	}
 }
